d51d2fcf6e51a47a53ea6555125ca498490c0149
monta <montazeri68@gmail.com>
2023-05-11 19:10:35 +0330

    added retrofit

---
+++ src/main/java/com/tradingpedia/api/NumberLandApi.java
@@ -0,0 +1,5 @@
+package com.tradingpedia.api;
+
+public interface NumberLandApi {
+
+}
---
+++ src/main/java/com/tradingpedia/util/BotConfig.java
@@ -0,0 +1,6 @@
+package com.tradingpedia.util;
+
+public class BotConfig {
+    public static final String BOT_TOKEN = "6089300074:AAFSK_Kqj-lyEsRHyYtKvosMAfnaF5XuO6Y";
+    public static final String BOT_USERNAME = "NumberLandVirtualNumberBot";
+}
